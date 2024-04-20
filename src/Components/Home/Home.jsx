21e84f072eac39ba8b84a464e80aea38bd7e2108
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-20 07:31:33 +0600

    added react and flowbite

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>this is home componets</h2>
+    </div>
+  );
+};
+
+export default Home;
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,7 +1,10 @@
+import Home from "../Components/Home/Home";
+
 const Root = () => {
   return (
     <div>
       <h2>This is My Root component</h2>
+      <Home></Home>
     </div>
   );
 };
