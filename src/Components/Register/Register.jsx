49366e903845ad53909109bede4925b9f65b0387
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-04-22 11:44:06 +0600

    if register successful then Form all field will be clear

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 const Register = () => {
-  // const [name, setName] = useState(null);
-  // const [email, setEmail] = useState(null);
-  // const [password, setPassword] = useState(null);
-  // console.log(name,email,password);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(name, email, password);
 
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
@@ -21,16 +22,16 @@ const Register = () => {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    const name = e.target.name.value;
-    // setName(name);
-    const email = e.target.email.value;
-    // setEmail(email);
-    const password = e.target.password.value;
-    // setPassword(password);
+    // const name = e.target.name.value;
+    // // setName(name);
+    // const email = e.target.email.value;
+    // // setEmail(email);
+    // const password = e.target.password.value;
+    // // setPassword(password);
 
     if (password.length < 6) {
       // console.log("your password must be 6 length");
-      setPassError("your password must be 6 length");
+      setPassError(" password must be 6 character");
       return;
     }
 
@@ -45,6 +46,9 @@ const Register = () => {
         const user = userCredential.user;
         console.log(user);
         setSuccess("Created Successfully");
+        setName("");
+        setEmail("");
+        setPassword("");
 
         // ...
       })
@@ -55,6 +59,8 @@ const Register = () => {
         // ..
       });
   };
+
+  console.log("after submit:", name, email, password);
 
   return (
     <div className="h-full z-30">
@@ -67,19 +73,19 @@ const Register = () => {
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input type="Text" name="name" placeholder="name" className="input input-bordered" required />
+                <input onChange={(e) => setName(e.target.value)} type="Text" name="name" value={name} placeholder="name" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" value={email} placeholder="email" className="input input-bordered" required />
               </div>
               <div className=" relative form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type={showpass ? "password" : "text"} name="password" placeholder="password" className="  input input-bordered" required />
+                <input onChange={(e) => setPassword(e.target.value)} type={showpass ? "password" : "text"} name="password" value={password} placeholder="password" className="  input input-bordered" required />
 
                 <span onClick={() => handleShowPass()} className=" absolute cursor-pointer right-2 bottom-4">
                   {showpass ? <FaRegEye /> : <FaRegEyeSlash />}
@@ -94,9 +100,9 @@ const Register = () => {
         </div>
       </div>
       {/* showing error when error will be occured */}
-      {error && <p className=" absolute -bottom-7 right-10 p-2 bg-red-700 text-md font-semibold text-white rounded-xl my-2 text-center">{error} </p>}
-      {success && <p className=" absolute -bottom-7 right-10 p-2 bg-green-700 text-md font-semibold text-white rounded-xl my-2 text-center">{success} </p>}
-      {passError && <p className="absolute -bottom-7 right-10 p-2 bg-red-700 text-md font-semibold text-white rounded-xl my-2 text-center">{passError} </p>}
+      {error && <p className=" absolute bottom-0 right-10 p-2 bg-red-700 text-md font-semibold text-white rounded-xl my-2 text-center">{error} </p>}
+      {success && <p className=" absolute bottom-0 right-10 p-2 bg-green-700 text-md font-semibold text-white rounded-xl my-2 text-center">{success} </p>}
+      {passError && <p className="absolute bottom-0 right-10 p-2 bg-red-700 text-md font-semibold text-white rounded-xl my-2 text-center">{passError} </p>}
     </div>
   );
 };
